37831f335d876fb3b562247e8cf981c9c48ecbb6
ngocdiemm <ngocdiem.le.2803@gmail.com>
2023-11-03 22:57:02 +0700

    Register

--- MyAirQuality/app/src/main/java/com/uit/myairquality/Register.java
+++ MyAirQuality/app/src/main/java/com/uit/myairquality/Register.java
@@ -2,13 +2,75 @@ package com.uit.myairquality;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class Register extends AppCompatActivity {
-
+    EditText username, password, repassword, email;
+    Button signup, signin;
+    Database DB;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        username = (EditText)findViewById(R.id.Username);
+        password = (EditText)findViewById(R.id.Password);
+        repassword = (EditText)findViewById(R.id.ReType);
+        email = (EditText)findViewById(R.id.Email);
+        signup = (Button) findViewById(R.id.btn_signup);
+        signin = (Button) findViewById(R.id.btn_back);
+        DB = new Database(this,"Final_app.db",null,1);
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+                String Email = email.getText().toString();
+                if(user.equals("")||pass.equals("")||repass.equals("")||Email.equals(""))
+                    Toast.makeText(Register.this, "Vui lòng điền hết tất cả các trường", Toast.LENGTH_SHORT).show();
+                else{
+                    if(pass.equals(repass)){
+                        Boolean checkuser = DB.checkusername(user);
+                        if(checkuser==false){
+                            Boolean insert = DB.insertData(user, pass, Email);
+                            if(insert==true){
+                                Toast.makeText(Register.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+//                                Intent intent = new Intent(getApplicationContext(),AboutActivity.class);
+//                                startActivity(intent);
+                            }else{
+                                Toast.makeText(Register.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(Register.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(Register.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
+                    }
+                } }
+        });
+
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
+
