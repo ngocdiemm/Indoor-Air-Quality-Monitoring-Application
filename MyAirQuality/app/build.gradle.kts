3b81eb5618f2f019d7c595d0ee0a7978ccfe9a58
ngocdiemm <ngocdiem.le.2803@gmail.com>
2023-11-05 06:45:40 +0700

    Homepage

--- MyAirQuality/app/build.gradle.kts
+++ MyAirQuality/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.uit.myairquality"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.uit.myairquality"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- MyAirQuality/app/src/main/java/com/uit/myairquality/Homepage.java
+++ MyAirQuality/app/src/main/java/com/uit/myairquality/Homepage.java
@@ -2,7 +2,10 @@ package com.uit.myairquality;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        //Mở màn hình LogIn
+        Button btnLogin = findViewById(R.id.appCompatButton1);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Homepage.this, LogIn.class);
+                startActivity(intent);
+            }
+        });
+        //Mở màn hình Register
+        Button btnRegister = findViewById(R.id.appCompatButton);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Homepage.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
